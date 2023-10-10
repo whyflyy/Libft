c1e5f08880e3983b52da2d0b857c432eb09177ac
Joao Cavadas Ribeiro <jcavadas@c1r9s3.42porto.com>
2023-10-10 17:50:55 +0100

    Finished the project and validated everything with francinette

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 09:41:18 by jcavadas          #+#    #+#             */
-/*   Updated: 2023/10/10 12:36:58 by jcavadas         ###   ########.fr       */
+/*   Updated: 2023/10/10 16:00:05 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,7 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
